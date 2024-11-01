6b35b98cfca12d2a507841ccc42d9fd7e40570de
RanaHany265 <you@ranahanymohamed265@gmail.com>
2024-11-01 11:13:36 +0000

    66

--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -20,16 +20,16 @@ class BaseCaching():
                                                                                     """
                                                                                             print("Current cache:")
                                                                                                     for key in sorted(self.cache_data.keys()):
-                                                                                                                    print("{}: {}".format(key, self.cache_data.get(key)))
+                                                                                                                print("{}: {}".format(key, self.cache_data.get(key)))
 
-                                                                                                                        def put(self, key, item):
-                                                                                                                                    """Adds an item in the cache.
-                                                                                                                                            """
-                                                                                                                                                    error_msg = "put must be implemented in your cache class"
-                                                                                                                                                            raise NotImplementedError(error_msg)
+                                                                                                                    def put(self, key, item):
+                                                                                                                            """Adds an item in the cache.
+                                                                                                                                    """
+                                                                                                                                            error_msg = "put must be implemented in your cache class"
+                                                                                                                                                    raise NotImplementedError(error_msg)
 
-                                                                                                                                                            def get(self, key):
-                                                                                                                                                                        """Retrieves an item by key.
-                                                                                                                                                                                """
-                                                                                                                                                                                        error_msg = "get must be implemented in your cache class"
-                                                                                                                                                                                                raise NotImplementedError(error_msg)
+                                                                                                                                                        def get(self, key):
+                                                                                                                                                                """Retrieves an item by key.
+                                                                                                                                                                        """
+                                                                                                                                                                                error_msg = "get must be implemented in your cache class"
+                                                                                                                                                                                        raise NotImplementedError(error_msg)
